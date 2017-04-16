2c2c145f79b229eeb3b980e667993f9f44be55f8
guillochon <guillochon@gmail.com>
2017-04-15 21:32:49 -0700

    Update.

--- catexplorer/main.py
+++ catexplorer/main.py
@@ -8,6 +8,7 @@ import importlib.util
 
 from fuzzywuzzy import process
 
+from astropy.time import Time as astrotime
 from bokeh.layouts import column, row
 from bokeh.models import Button, Range1d, ColumnDataSource
 from bokeh.models.widgets import TextInput
@@ -67,7 +68,7 @@ with open('/root/astrocats/astrocats/supernovae/output/names.min.json') as f:
 names = list(names.keys())
 unames = [x.upper() for x in names]
 
-bands = ['U', 'B', 'V', 'R', 'I']
+bands = plotting.bandcodes
 ubands = [x.upper() for x in bands]
 
 nds = []
@@ -101,14 +102,18 @@ def callback():
         if band:
             newband = band.upper()
             choices = []
-            for ui, un in enumerate(ubands):
+            for ui, un in enumerate(bands):
                 if un == newband:
                     finalband = bands[ui]
                     break
-                if newband in un:
+                if newband.upper() in un.upper():
                     choices.append(bands[ui])
             if not finalband:
-                finalband = process.extractOne(band, choices)[0]
+                extract = process.extractOne(band, choices)
+                if extract:
+                    finalband = extract[0]
+                else:
+                    finalband = band
         if not finalband:
             finalband = 'V'
         newbands.append(finalband)
@@ -121,51 +126,70 @@ def callback():
         event = event[list(event.keys())[0]]
 
         for band in newbands:
+            print(band)
             new_data = {}
             new_data['x'] = []
             new_data['y'] = []
             new_data['band'] = band
+            new_data['maxmjd'] = astrotime(event['maxdate'][0]['value'].replace('/', '-')).mjd if 'maxdate' in event else ''
+            new_data['stretch'] = (1.0 / (
+                1.0 + float(event['redshift'][0]['value']))) if (
+                    'redshift' in event) else 1.0
+            new_data['distancemod'] = ((float(event['maxappmag'][0]['value']) -
+                float(event['maxabsmag'][0]['value']))) if ('maxabsmag' in event and 'maxappmag' in event) else 0.0
             for photo in event['photometry']:
                 if photo.get('band', '') != band:
                     continue
+                if 'time' not in photo or 'magnitude' not in photo:
+                    continue
                 new_data['x'].append(float(photo['time']))
                 new_data['y'].append(float(photo['magnitude']))
-            nds.append(copy.deepcopy(new_data))
+            nds.append(new_data)
 
+    maxmjds = []
     if len(newnames) > 1:
-        maxmjd = []
-        for nd in nds:
+        for ndi, nd in enumerate(nds):
+            maxmjds.append(nd['maxmjd'])
+            if maxmjds[ndi]:
+                continue
             if not len(nd['x']):
                 continue
-            maxmjd.append(nd['x'][nd['y'].index(min(nd['y']))])
+            maxmjds[ndi] = nd['x'][nd['y'].index(min(nd['y']))]
         for ndi, nd in enumerate(nds):
             if not len(nd['x']):
                 continue
-            nds[ndi]['x'] = [x - maxmjd[ndi] for x in nd['x']]
+            nds[ndi]['x'] = [x - maxmjds[ndi] for x in nd['x']]
+
+    for ndi, nd in enumerate(nds):
+        if not len(nd['x']) or not len(nd['y']):
+            continue
+        nds[ndi]['y'] = [x - nd['distancemod'] for x in nd['y']]
+        nds[ndi]['x'] = [x*nd['stretch'] for x in nd['x']]
 
     nxs = []
     nys = []
     for nd in nds:
         nxs.extend(nd['x'])
         nys.extend(nd['y'])
-    x_buf = 0.1*(max(nxs) - min(nxs))
-    y_buf = 0.1*(max(nys) - min(nys))
+    if len(nxs) and len(nys):
+        x_buf = 0.1*(max(nxs) - min(nxs))
+        y_buf = 0.1*(max(nys) - min(nys))
 
-    p.x_range.start = min(nxs) - x_buf
-    p.x_range.end = max(nxs) + x_buf
-    p.y_range.start = max(nys) + y_buf
-    p.y_range.end = min(nys) - y_buf
+        p.x_range.start = min(nxs) - x_buf
+        p.x_range.end = max(nxs) + x_buf
+        p.y_range.start = max(nys) + y_buf
+        p.y_range.end = min(nys) - y_buf
 
     mld = {}
     mldf = {}
     for nd in nds:
-        mld.setdefault('xs',[]).append(nd['x'])
-        mld.setdefault('ys',[]).append(nd['y'])
-        mld.setdefault('colors',[]).append(plotting.bandcolorf(nd['band']))
+        mld.setdefault('xs',[]).append(nd['x'] if len(nd['x']) else [0.0])
+        mld.setdefault('ys',[]).append(nd['y'] if len(nd['y']) else [0.0])
+        mld.setdefault('colors',[]).append(plotting.bandcolorf(nd['band']) if len(nd['x']) else 'white')
         mld.setdefault('lws',[]).append(2)
 
-        mldf.setdefault('xs',[]).extend(nd['x'])
-        mldf.setdefault('ys',[]).extend(nd['y'])
+        mldf.setdefault('xs',[]).extend(nd['x'] if len(nd['x']) else [])
+        mldf.setdefault('ys',[]).extend(nd['y'] if len(nd['y']) else [])
         mldf.setdefault('colors',[]).extend([plotting.bandcolorf(nd['band']) for x in range(len(nd['x']))])
         
     mlobs.data_source.data = mld
